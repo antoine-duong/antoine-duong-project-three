0060cd8806444289356c1617fde9c42b07a467b2
Antoine <tadgithub@gmail.com>
2022-05-03 23:17:27 -0400

    Add sandbox to iframe

--- src/Components/RankList.js
+++ src/Components/RankList.js
@@ -1,7 +1,7 @@
 const RankList = (props)=>{
     return (
         <div className="rankListContainer">
-            <iframe src={props.snapshot.url} title={props.inputDate}>Your browser doesn't support iframe.</iframe>
+            <iframe src={props.snapshot.url} title={props.inputDate} sandbox>Your browser doesn't support iframe.</iframe>
         </div>
     );
 }
