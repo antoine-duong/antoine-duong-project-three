7ec1a8d7682377bb5166a4ae65a4a83ada11aa75
Antoine <tadgithub@gmail.com>
2022-05-05 11:01:26 -0400

    Fix typo

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -3,7 +3,7 @@ const Article = ()=>{
         <article className='mt-3 mx-3 columns is-family-code is-size-6 is-size-7-mobile'>
             <div className='column has-background-link-light has-text-left m-1'>
                 <p className="mb-1">At the moment of this writing, this app is designed specifically as an educational tool for those who wish to learn about the cryptocurrency market (<em>hover your cursor over the site name above for more info</em>).</p>
-                <p className=" mb-1">As much as most crypto enthusiasts hate to admit, this space is quite crowded with scammers, shillers and piles of dead projects. If you find yourself getting bitten by the <a href="https://www.investopedia.com/terms/p/panicbuying.asp" className="has-text-black">FOMO</a> bug, simply cast a glance into the past and experience the votality of the market for yourself.</p>
+                <p className=" mb-1">As much as most crypto enthusiasts hate to admit it, this space is quite crowded with scammers, shillers and piles of dead projects. If you find yourself getting bitten by the <a href="https://www.investopedia.com/terms/p/panicbuying.asp" className="has-text-black">FOMO</a> bug, simply cast a glance into the past and experience the votality of the market for yourself.</p>
             </div>
             <div className='column has-background-primary-light m-1'>
                 <p className="has-text-right is-italic">That fear of missing out on things makes you miss out on everything.</p>
